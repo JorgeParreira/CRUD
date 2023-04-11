569e313d23f950ca888f179befe5420a7868c62d
JorgeParreira <jorge.parreira@gmail.com>
2023-04-11 09:51:02 +0100

    Sidebar updating.

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -9,7 +9,7 @@ export default props =>
                 <i className="fa fa-home"></i> Início 
             </Link>
             <Link to="/users">
-                <i className="fa fa-users"></i> Usuários
+                <i className="fa fa-users"></i> Utilizadores
             </Link>
         </nav>
     </aside>
